7508d577de7ddd0fd1855a17ee1a7fe7a9ff2e00
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-05-10 06:46:37 -0400

    use new V2 download async

--- full-stack-tests/native-app/src/frontend/hub/Download.test.ts
+++ full-stack-tests/native-app/src/frontend/hub/Download.test.ts
@@ -3,14 +3,13 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { assert } from "chai";
-import { BeDuration, GuidString, StopWatch } from "@bentley/bentleyjs-core";
+import { GuidString, StopWatch } from "@bentley/bentleyjs-core";
 import { ElectronApp } from "@bentley/electron-manager/lib/ElectronFrontend";
 import { IModelVersion, SyncMode } from "@bentley/imodeljs-common";
 import { BriefcaseConnection, NativeApp } from "@bentley/imodeljs-frontend";
 import { ProgressInfo } from "@bentley/itwin-client";
 import { usingOfflineScope } from "../HttpRequestHook";
 import { NativeAppTest } from "../NativeAppTest";
-import { timeLog } from "console";
 
 describe("NativeApp Download (#integration)", () => {
   let testProjectId: GuidString;
@@ -29,7 +28,7 @@ describe("NativeApp Download (#integration)", () => {
 
   after(async () => ElectronApp.shutdown());
 
-  it("Download Briefcase with progress events (#integration)", async () => {
+  it.only("Download Briefcase with progress events (#integration)", async () => {
     let events = 0;
     let loaded = 0;
     let total = 0;
@@ -62,12 +61,9 @@ describe("NativeApp Download (#integration)", () => {
 
   it.only("Should be able to cancel download (#integration)", async () => {
     const locTestIModelId = await NativeAppTest.getTestIModelId(testProjectId, "Stadium Dataset 1");
-    console.log("request");
 
     const watch = new StopWatch("", true);
 
-    const cancelCalled = false;
-    const requestVal = false;
     let downloadAborted = false;
     let events = 0;
     let loaded = 0;
@@ -81,22 +77,22 @@ describe("NativeApp Download (#integration)", () => {
         loaded = progress.loaded;
         events++;
         calls.push(`progress ${progress.loaded} ${progress.total} elapsed = ${watch.elapsedSeconds}`);
+        if (events > 2)
+          void downloader.requestCancel();
       });
-    console.log("requested");
 
     try {
-      console.log("waiting ", watch.elapsedSeconds);
       await downloader.downloadPromise;
       console.log("done download ", watch.elapsedSeconds);
     } catch (err) {
+      assert.isTrue(err.message.includes("abort"));
       downloadAborted = true;
     }
     console.log("after wait ", watch.elapsedSeconds);
     console.log("calls = ", calls);
+    console.log("filename = ", downloader.fileName);
     await NativeApp.deleteBriefcase(downloader.fileName);
     assert.isTrue(events !== 0);
-    assert.isTrue(cancelCalled, "cancel should be called");
-    assert.isTrue(requestVal, "cancelRequest should succeed");
     assert.isTrue(downloadAborted, "download should abort");
   });
 
